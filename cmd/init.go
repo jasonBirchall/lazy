7295e4ab9a649107b677485f0039c5e9bd51a90b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-11-22 16:07:43 +0000

    :hammer: Add init command to create configuration file
    Introduced a new `init` command using Cobra, which generates a `.lazycommit.yaml` configuration file in the user's home directory. This facilitates the initial setup process by providing users with a basic configuration template upon execution.

---
+++ cmd/init.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+var configContent = `
+# Configuration file for lazycommit
+# Add your configuration settings here
+`
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initializes the configuration file in the user's Linux profile",
+	Long: `This command initializes a configuration file named .lazycommit.yaml
+in the user's home directory.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println("Error getting home directory:", err)
+			return
+		}
+
+		configFilePath := filepath.Join(homeDir, ".lazycommit.yaml")
+		err = os.WriteFile(configFilePath, []byte(configContent), 0644)
+		if err != nil {
+			fmt.Println("Error writing config file:", err)
+			return
+		}
+
+		fmt.Println("Configuration file created at:", configFilePath)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
